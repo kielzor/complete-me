f3e5e88a6c9d581a88c03a1a16a55d84ea4e50de
Kiel Dehnert <kielzor@gmail.com>
2018-07-14 17:58:09 -0600

    Add find and suggest methods

--- test/trie-test.js
+++ test/trie-test.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { expect } = require('chai');
 const Trie = require('../lib/trie.js');
 
@@ -11,22 +12,83 @@ describe('Trie', () => {
 
   it('should exist', () => {
     expect(trie).to.exist;
-  })
-    
-  it('should have a rootNode node defaulted to null', () => {
-    expect(trie.rootNode).to.equal(null);
   });
 
- it('should take in an word and split it into an array of letters', () =>  {
-  console.log(new Trie('pizza'))
- });
-    
+  it('2. trie should create an empty root node', () => {
+    expect(trie.head.value).to.equal('')
+  });
+      
   describe('insert', () => {
+
     it('should be able to add a node to the Trie', () => {
       trie.insert('pizza');
       
-      expect(trie.rootNode.value).to.equal('pizza');
+      expect(trie.head.child.p.value).to.deep.equal('p');
+      expect(trie.head.child.p.child.i.value).to.deep.equal('i');
+    });
+
+    it('Should be able the value is a word', () => {
+
+
+      expect(trie.head.child).to.deep.equal({});
+
+      trie.insert('taco');
+
+      expect(
+        trie.head
+        .child.t
+        .child.a
+        .child.c
+        .child.o
+        .isWord
+      ).to.equal('taco');
+    })
+  })
+
+  describe('count', () => {
+    it('Should keep track of the amount of words imported', () => {
+      trie.insert('pizza');
+      trie.insert('taco');
+
+      expect(trie.count()).to.equal(2);
+
+      trie.insert('burger');
+
+      expect(trie.count()).to.equal(3);      
+    })
+
+    it('Should not create a new word if it is a duplicate', () => {
+      trie.insert('pizza');
+      trie.insert('pizza');
+
+      expect(trie.count()).to.equal(1)
     });
   })
-});
+
+  describe('find and suggest', () => {
+    it('Should suggest words', () =>{ 
+      trie.insert('pizza');
+      trie.insert('pineapple')
+
+      expect(trie.find('pi')).to.deep.equal(['pizza', 'pineapple']);
+    });
+
+    it('Should not suggest if the prefix does not include letters', () => {
+      trie.insert('pizza');
+      trie.insert('juice')
+
+      expect(trie.find('pi')).to.deep.equal(['pizza']);
+    })
+  })
+
+  describe('populate', () => {
+    const text = "/usr/share/dict/words";
+    const dictionary = fs.readFileSync(text).toString().trim().split('\n');
     
+    it('Should populate the dictionary', () => {
+      trie.populate(dictionary);
+
+      expect(trie.count()).to.equal(234371)
+    })  
+  })
+});

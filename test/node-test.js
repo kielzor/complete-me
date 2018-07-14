f4298b259ff5948e1e5cf7429fc7722aee9fd150
Kiel Dehnert <kielzor@gmail.com>
2018-07-14 10:42:26 -0600

    Fix dependency bug add locus debugger

--- test/node-test.js
+++ test/node-test.js
@@ -12,11 +12,11 @@ describe('NODE', () => {
     expect(node).to.exist;
   })
 
-  it('should default next to null', () => {
-    expect(node.next).to.equal(null);
+  it('should default next node to null', () => {
+    expect(node.child).to.equal(null);
   })
 
   it('should take data and assign it to data prop', () => {
-    expect(node.data).to.equal('pizza');
+    expect(node.value).to.equal('pizza');
   })
 })
--- test/trie-test.js
+++ test/trie-test.js
@@ -1,10 +1,32 @@
 const { expect } = require('chai');
-const Trie = require('../lib/node.js');
+const Trie = require('../lib/trie.js');
+
 
 describe('Trie', () => {
-  let trie = new Trie('pizza');
-  
-    it('should exist', () => {
-      expect(trie).to.exist;
-    })
+  let trie;
+
+  beforeEach(() => {
+    trie = new Trie();
   });
+
+  it('should exist', () => {
+    expect(trie).to.exist;
+  })
+    
+  it('should have a rootNode node defaulted to null', () => {
+    expect(trie.rootNode).to.equal(null);
+  });
+
+ it('should take in an word and split it into an array of letters', () =>  {
+  console.log(new Trie('pizza'))
+ });
+    
+  describe('insert', () => {
+    it('should be able to add a node to the Trie', () => {
+      trie.insert('pizza');
+      
+      expect(trie.rootNode.value).to.equal('pizza');
+    });
+  })
+});
+    
